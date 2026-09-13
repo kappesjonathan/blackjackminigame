0a833377e46a6ddb99248682bd3cc5ff133e1be3
Jonathan <jonathankappes@hotmail.com>
2026-09-13 17:05:45 -0300

    Add files via upload

--- blackjack/ventana.py
+++ blackjack/ventana.py
@@ -1,6 +1,42 @@
-from tkinter import Tk
+from tkinter import *
+from tkinter import ttk, font
 
-print('tkinter is available')
-print('Standard GUI toolkit for Python')
+# Crea una clase Python para definir el interfaz de usuario de
+# la aplicación. Cuando se cree un objeto del tipo 'Aplicacion'
+# se ejecutará automáticamente el método __init__() qué 
+# construye y muestra la ventana con todos sus widgets: 
 
-raiz = Tk()
+class Aplicacion():
+    def __init__(self):
+        raiz = Tk()
+        raiz.geometry('700x500')
+        raiz.configure(bg = 'green')
+        raiz.title('Aplicación')
+
+        fuente = font.Font(weight='bold')
+        etiq1 = ttk.Label(raiz, text="Usuario:", font=fuente)
+        etiq2 = ttk.Label(raiz, text="Contraseña:", font=fuente)
+
+        usuario = StringVar()
+        clave = StringVar()
+
+        ttk.Button(raiz, text='Salir', 
+                   command=raiz.destroy).pack(side=BOTTOM)
+        raiz.mainloop()
+
+# Define la función main() que es en realidad la que indica 
+# el comienzo del programa. Dentro de ella se crea el objeto 
+# aplicación 'mi_app' basado en la clase 'Aplicación':
+
+def main():
+    mi_app = Aplicacion()
+    return 0
+
+# Mediante el atributo __name__ tenemos acceso al nombre de un
+# un módulo. Python utiliza este atributo cuando se ejecuta
+# un programa para conocer si el módulo es ejecutado de forma
+# independiente (en ese caso __name__ = '__main__') o es 
+# importado:
+
+if __name__ == '__main__':
+    main()
